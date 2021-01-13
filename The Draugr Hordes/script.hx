e813c2142c65794777ebb23f47fe3365e72081a9
joeylmaalouf <joeylmaalouf@gmail.com>
2021-01-13 15:31:17 -0500

    extra wave for easing into buildup

--- The Draugr Hordes/script.hx
+++ The Draugr Hordes/script.hx
@@ -14,6 +14,7 @@ var wavePaths = [
 var waveUnitTypes = [Unit.Death, Unit.Valkyrie, Unit.UndeadGiant];
 var waveContents = [ // number of each unit type to send in each wave
 	[2, 0, 0],
+	[3, 0, 0],
 	[4, 0, 0],
 	[4, 1, 0],
 	[4, 2, 0],
